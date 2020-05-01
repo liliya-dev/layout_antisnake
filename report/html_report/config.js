134f305381d7e79727151db9380ad53c34971c53
Liliia <chuiko1112@gmail.com>
2020-05-01 13:14:12 +0300

    Updates

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document_300.png",
-        "test": "../bitmaps_test/20200310-081030/Entire_document_300.png",
+        "test": "../bitmaps_test/20200501-130506/Entire_document_300.png",
         "selector": "document",
         "fileName": "Entire_document_300.png",
         "label": "Entire document",
@@ -14,23 +14,14 @@ report({
         "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
         "expect": 0,
         "viewportLabel": "300",
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": 0,
-            "height": -1600
-          },
-          "misMatchPercentage": "10.96",
-          "analysisTime": 41
-        },
-        "diffImage": "../bitmaps_test/20200310-081030/failed_diff_Entire_document_300.png"
+        "error": "Reference file not found /Users/lili4ka/Desktop/projects/layout_antisnake/backstop_data/bitmaps_reference/Entire_document_300.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document_450.png",
-        "test": "../bitmaps_test/20200310-081030/Entire_document_450.png",
+        "test": "../bitmaps_test/20200501-130506/Entire_document_450.png",
         "selector": "document",
         "fileName": "Entire_document_450.png",
         "label": "Entire document",
@@ -40,23 +31,14 @@ report({
         "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
         "expect": 0,
         "viewportLabel": "450",
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": 0,
-            "height": -1600
-          },
-          "misMatchPercentage": "11.01",
-          "analysisTime": 62
-        },
-        "diffImage": "../bitmaps_test/20200310-081030/failed_diff_Entire_document_450.png"
+        "error": "Reference file not found /Users/lili4ka/Desktop/projects/layout_antisnake/backstop_data/bitmaps_reference/Entire_document_450.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document_600.png",
-        "test": "../bitmaps_test/20200310-081030/Entire_document_600.png",
+        "test": "../bitmaps_test/20200501-130506/Entire_document_600.png",
         "selector": "document",
         "fileName": "Entire_document_600.png",
         "label": "Entire document",
@@ -66,23 +48,14 @@ report({
         "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
         "expect": 0,
         "viewportLabel": "600",
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": 0,
-            "height": -700
-          },
-          "misMatchPercentage": "21.81",
-          "analysisTime": 65
-        },
-        "diffImage": "../bitmaps_test/20200310-081030/failed_diff_Entire_document_600.png"
+        "error": "Reference file not found /Users/lili4ka/Desktop/projects/layout_antisnake/backstop_data/bitmaps_reference/Entire_document_600.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document_750.png",
-        "test": "../bitmaps_test/20200310-081030/Entire_document_750.png",
+        "test": "../bitmaps_test/20200501-130506/Entire_document_750.png",
         "selector": "document",
         "fileName": "Entire_document_750.png",
         "label": "Entire document",
@@ -92,23 +65,14 @@ report({
         "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
         "expect": 0,
         "viewportLabel": "750",
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": 0,
-            "height": -700
-          },
-          "misMatchPercentage": "21.90",
-          "analysisTime": 100
-        },
-        "diffImage": "../bitmaps_test/20200310-081030/failed_diff_Entire_document_750.png"
+        "error": "Reference file not found /Users/lili4ka/Desktop/projects/layout_antisnake/backstop_data/bitmaps_reference/Entire_document_750.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document_900.png",
-        "test": "../bitmaps_test/20200310-081030/Entire_document_900.png",
+        "test": "../bitmaps_test/20200501-130506/Entire_document_900.png",
         "selector": "document",
         "fileName": "Entire_document_900.png",
         "label": "Entire document",
@@ -118,23 +82,14 @@ report({
         "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
         "expect": 0,
         "viewportLabel": "900",
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": 0,
-            "height": -400
-          },
-          "misMatchPercentage": "32.68",
-          "analysisTime": 67
-        },
-        "diffImage": "../bitmaps_test/20200310-081030/failed_diff_Entire_document_900.png"
+        "error": "Reference file not found /Users/lili4ka/Desktop/projects/layout_antisnake/backstop_data/bitmaps_reference/Entire_document_900.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document_1050.png",
-        "test": "../bitmaps_test/20200310-081030/Entire_document_1050.png",
+        "test": "../bitmaps_test/20200501-130506/Entire_document_1050.png",
         "selector": "document",
         "fileName": "Entire_document_1050.png",
         "label": "Entire document",
@@ -144,23 +99,14 @@ report({
         "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
         "expect": 0,
         "viewportLabel": "1050",
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": 0,
-            "height": -400
-          },
-          "misMatchPercentage": "32.77",
-          "analysisTime": 93
-        },
-        "diffImage": "../bitmaps_test/20200310-081030/failed_diff_Entire_document_1050.png"
+        "error": "Reference file not found /Users/lili4ka/Desktop/projects/layout_antisnake/backstop_data/bitmaps_reference/Entire_document_1050.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document_1200.png",
-        "test": "../bitmaps_test/20200310-081030/Entire_document_1200.png",
+        "test": "../bitmaps_test/20200501-130506/Entire_document_1200.png",
         "selector": "document",
         "fileName": "Entire_document_1200.png",
         "label": "Entire document",
@@ -170,16 +116,7 @@ report({
         "referenceUrl": "https://mate-academy.github.io/layout_solutions/antisnake/",
         "expect": 0,
         "viewportLabel": "1200",
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": 0,
-            "height": -400
-          },
-          "misMatchPercentage": "32.84",
-          "analysisTime": 74
-        },
-        "diffImage": "../bitmaps_test/20200310-081030/failed_diff_Entire_document_1200.png"
+        "error": "Reference file not found /Users/lili4ka/Desktop/projects/layout_antisnake/backstop_data/bitmaps_reference/Entire_document_1200.png"
       },
       "status": "fail"
     }
